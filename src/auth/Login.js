602726676901fa795b194736353bf0d0bba2d4ad
Sebastian <sebastian.molina01@alumnos.ucn.cl>
2025-01-20 11:11:02 -0300

    alerta error iniciar sesion

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -3,80 +3,82 @@ import './Login.css';
 import axios from 'axios';
 
 function Login() {
-    const [ auth, setAuth ] = useState({ username: '', password: ''})
+    const [auth, setAuth] = useState({ username: '', password: '' });
 
     const handleSubmit = async (e) => {
-
         e.preventDefault();
 
-        try{
+        try {
             const resp = await axios.post(`http://localhost:3000/api/v1/auth/signin`, {
                 username: auth.username.trim(),
-                password: auth.password
+                password: auth.password,
             });
             const { access_token } = resp.data;
             localStorage.setItem('token', access_token);
-            localStorage.setItem('id_user', resp.data.id );
-    
+            localStorage.setItem('id_user', resp.data.id);
+
             if (access_token) {
                 localStorage.setItem('session', 'exito');
                 window.location.replace('/home');
             } else {
-                console.log("Usuario no existe")
+                console.log("Usuario no existe");
             }
-        } catch (error){
-            console.error('Error en el login:', error);
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                alert('Usuario o contraseña incorrectos');
+            } else {
+                console.error('Error en el login:', error);
+                alert('Ocurrió un error. Por favor, inténtalo de nuevo más tarde.');
+            }
         }
     };
 
     const handleChange = (e) => {
         setAuth({
             ...auth,
-            [e.target.name]: e.target.value
-        })
-    }
-    
+            [e.target.name]: e.target.value,
+        });
+    };
+
     if (localStorage.getItem('error')) {
         localStorage.removeItem('error');
-        setTimeout(function() {
-          alert('Error en la sesión, ingresa nuevamente');
-        }, 500); 
+        setTimeout(function () {
+            alert('Error en la sesión, ingresa nuevamente');
+        }, 500);
     }
 
     if (localStorage.getItem('logout')) {
         localStorage.removeItem('logout');
-        setTimeout(function() {
-          alert('Has cerrado sesión');
-        }, 500); 
+        setTimeout(function () {
+            alert('Has cerrado sesión');
+        }, 500);
     }
 
     return (
         <div>
             <header>
-                <img src='/LOGO_GM_EXPRESS.png' className="app-logo" alt="logo" />
+                <img src="/LOGO_GM_EXPRESS.png" className="app-logo" alt="logo" />
             </header>
             <div className="ActualBody">
                 <div className="Login">
                     <h2>Iniciar Sesión</h2>
                     <form onSubmit={handleSubmit}>
                         <div>
-                            
                             <input
                                 type="text"
                                 name="username"
                                 value={auth.username}
                                 onChange={handleChange}
-                                placeholder='Nombre de Usuario: '
+                                placeholder="Nombre de Usuario: "
                             />
                         </div>
                         <div>
-   
                             <input
                                 type="password"
                                 name="password"
                                 value={auth.password}
                                 onChange={handleChange}
-                                placeholder='Contraseña: '
+                                placeholder="Contraseña: "
                             />
                         </div>
                         <button type="submit">Login</button>

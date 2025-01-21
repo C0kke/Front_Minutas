84585731f13b725cb4debbc580ae2a9a0bc0169b
Cokke <jorgerandomgamer@gmail.com>
2025-01-21 13:04:43 -0300

    historico actualizado

--- src/views/MenuHistorico.js
+++ src/views/MenuHistorico.js
@@ -1,35 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import axios from 'axios';
 import './styles/menuhistorico.css';
 import Header from '../components/Header';
-import { Navigate } from 'react-router-dom';
+import TablaMinutaAprobacion from '../components/TablaMinuta';
 
 const MinutaLista = () => {
-  const [minutas, setMinutas] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [selectedSemana, setSelectedSemana] = useState(null);
-  const [selectedMinuta, setSelectedMinuta] = useState(null); 
   const [filter, setFilter] = useState('');
   const [allWeeks, setAllWeeks] = useState([]);
+  const [mostrarTabla, setMostrarTabla] = useState(false);
+  const [minutasAgrupadas, setMinutasAgrupadas] = useState({}); // Estado para las minutas agrupadas
+  const tablaMinutaRef = useRef(null);
   const token = localStorage.getItem('token');
+
   useEffect(() => {
     const fetchMinutas = async () => {
+      setLoading(true);
       try {
         const response = await axios.get('http://localhost:3000/api/v1/menudiario', {
           headers: { Authorization: `Bearer ${token}` }
         });
         const groupedMinutas = groupBySemana(response.data);
-        setMinutas(groupedMinutas);
-
-        const semanas = Object.keys(groupedMinutas);
-
-        setAllWeeks(semanas);
+        console.log(groupedMinutas)
+        setMinutasAgrupadas(groupedMinutas);
+        setAllWeeks(Object.keys(groupedMinutas));
       } catch (error) {
         setError("Error al cargar las minutas");
         console.error('Error fetching minutas:', error);
-        localStorage.setItem('error', 'error en la sesion')
-        window.location.replace('/home');
       } finally {
         setLoading(false);
       }
@@ -40,34 +39,45 @@ const MinutaLista = () => {
 
   const groupBySemana = (data) => {
     return data.reduce((acc, minuta) => {
-      const semana = minuta.semana;
-      if (!acc[semana]) {
-        acc[semana] = [];
+      const fecha = new Date(minuta.fecha);
+      const semana = minuta.semana; 
+      const año = fecha.getFullYear();
+      const key = `${semana}-${año}`;
+      if (!acc[key]) {
+        acc[key] = [];
       }
-      acc[semana].push(minuta);
+      acc[key].push(minuta);
       return acc;
     }, {});
   };
 
   const handleSemanaClick = (semana) => {
     setSelectedSemana(semana);
-    setSelectedMinuta(null); 
-  };
-  
-  const handleMinutaClick = (minuta) => {
-    if (minuta.listaplatos && minuta.listaplatos.length > 0) {
-      setSelectedMinuta(minuta);
-    } else {
-      setSelectedMinuta({
-        ...minuta,
-        listaplatos: [], 
-      });
-    }
+    setMostrarTabla(true);
   };
 
   const filteredWeeks = allWeeks.filter((semana) =>
-    semana.includes(filter)
+    semana.toString().includes(filter)
   );
+
+  const obtenerAño = (minutas) => {
+    if(minutas && minutas.length > 0) {
+      return new Date(minutas[0].fecha).getFullYear()
+    }
+    return new Date().getFullYear();
+  }
+
+  const formatMinutasForTable = (minutas) => {
+      if (!minutas) return [];
+
+      return minutas.map(minuta => ({
+          ...minuta,
+          listaplatos: minuta.listaplatos.map(item => ({
+            ...item,
+            platoId: item.platoId._id
+          }))
+        }));
+  }
 
   if (loading) {
     return <div>Cargando...</div>;
@@ -79,7 +89,7 @@ const MinutaLista = () => {
 
   return (
     <div>
-      <Header/>
+      <Header />
       <div className="minuta-lista-container">
         <div className="filter-container">
           <h2>Selecciona una Semana</h2>
@@ -88,13 +98,12 @@ const MinutaLista = () => {
             className="filter-input"
             placeholder="Filtrar por semana..."
             value={filter}
-            onChange={(e) => setFilter(e.target.value)} // Actualiza el filtro
+            onChange={(e) => setFilter(e.target.value)}
           />
         </div>
 
         <div className="weeks-container">
-          {/* Mostrar las semanas filtradas */}
-          {filter && filteredWeeks.length > 0 ? (
+          {filteredWeeks.length > 0 ? (
             <div className="filtered-weeks">
               {filteredWeeks.map((semana) => (
                 <button
@@ -111,50 +120,15 @@ const MinutaLista = () => {
           )}
         </div>
 
-        {selectedSemana && (
-          <div className="minuta-details">
-            <h3>Minutas de la Semana {selectedSemana}</h3>
-            <table className="Minuta">
-              <thead>
-                <tr>
-                  <th>Nombre</th>
-                  <th>Fecha</th>
-                  <th>Estado</th>
-                </tr>
-              </thead>
-              <tbody>
-                {minutas[selectedSemana]?.map((minuta) => (
-                  <tr
-                    key={minuta._id}
-                    onClick={() => handleMinutaClick(minuta)} // Al hacer clic en la fila, se selecciona la minuta
-                    style={{ cursor: 'pointer' }}
-                  >
-                    <td>{minuta.nombre}</td>
-                    <td>{new Date(minuta.fecha).toLocaleDateString()}</td>
-                    <td>{minuta.estado}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+        {/* Mostrar la tabla al seleccionar una semana */}
+        {selectedSemana && mostrarTabla && (
+          <div className="tabla-minuta-container">
+            <TablaMinutaAprobacion
+              semana={{ _id: { semana: selectedSemana, año: obtenerAño(minutasAgrupadas[selectedSemana])}, menus: formatMinutasForTable(minutasAgrupadas[selectedSemana])}}
+              ref={tablaMinutaRef}
+            />
           </div>
         )}
-
-{selectedMinuta && (
-  <div className="platos-lista">
-    <h4>Platos de la Minuta: {selectedMinuta?.nombre}</h4>
-    <ul>
-      {selectedMinuta?.listaplatos?.length === 0 ? (
-        <p>No hay platos disponibles para este día.</p>
-      ) : (
-        selectedMinuta?.listaplatos.map((plato, index) => (
-          <li key={`${plato._id}-${index}`}> {/* Usa una clave única */}
-            {plato.nombre} - {plato.descripcion}
-          </li>
-        ))
-      )}
-    </ul>
-  </div>
-)}
       </div>
     </div>
   );
